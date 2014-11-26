9a8d654bf31866c1df90c612b9b15ef365674cb1
Marek Goczoł <marekgoczol@gmail.com>
2014-11-27 00:06:06 +0100

    readme file added

--- libs/app/index.js
+++ libs/app/index.js
@@ -1,22 +1,22 @@
 var test = require('test');
 
 var app = angular.module('app', [
-	'ui.router',
-	'test'
+    'ui.router',
+    'test'
 ]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/state1");
+    $urlRouterProvider.otherwise("/state1");
 
-	$stateProvider
-		.state('state1', {
-			url: '/state1',
-			template: '<app-test></app-test>'
-		})
-		.state('state2', {
-			url: '/state2',
-			template: '<p>State 2</p>'
-		});
+    $stateProvider
+        .state('state1', {
+            url: '/state1',
+            template: '<app-test></app-test>'
+        })
+        .state('state2', {
+            url: '/state2',
+            template: '<p>State 2</p>'
+        });
 
 });
 
---
+++ readme.md
@@ -0,0 +1,12 @@
+Angular starter application. 
+Using component, bower, grunt.
+
+* ``` sudo npm install ```
+
+* ``` bower install ```
+
+* ``` grunt vendors ```
+
+* ``` grunt ```
+
+then in browser: `0.0.0.0:8000`
